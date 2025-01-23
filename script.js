7fe3242095bae21e0b2e1c089838d7f6d442f8aa
amrimukh1 <amrimukh1@gmail.com>
2025-01-23 11:03:21 +0100

    Initial Commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const greetingElement= document.getElementById('greeting');
+const currenthour = new Date().getHours();
+//8AM-8
+//2:14
+
+let greetingMessage;
+if(currenthour<12){
+ 
+greetingMessage = 'Good Morning !!' ;
+
+
+}
+else if(currenthour<18){
+    greetingMessage = 'Good Afternoon !!'
+
+}else{
+
+greetingMessage = 'Good Evening !!';
+
+}
+
+greetingElement.textContent = greetingMessage;
+
+
+    function scrollToTop()
+      {
+        window.scrollTo({
+        top:0,
+        behaviour:'smooth',
+      })
+    }
+       //Greeting Message */
